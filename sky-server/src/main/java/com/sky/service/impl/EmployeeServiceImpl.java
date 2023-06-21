c3c0e43230084bb2e4346243b7b1b21731f94c5c
wangsong <384097785@qq.com>
2023-06-21 16:48:23 +0800

    实现新增员工业务

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -53,6 +59,39 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        //去数据库查找还是要用与数据库对应的entity，所以需要对象转换
+        Employee employee = new Employee();
+
+        //对象数据拷贝 把employeeDTO中的属性拷贝到employee中的属性，前提是要两个对象的属性名是一致的
+        BeanUtils.copyProperties(employeeDTO,employee);
+
+        //设置对象状态，默认是正常状态，1表示正常，0表示锁定 使用common中的常量类StatusConstant
+        employee.setStatus(StatusConstant.ENABLE);
+
+        //设置密码，默认密码是123456,使用common中的常量类PasswordConstant.DEFAULT_PASSWORD
+        // .getBytes() 方法是将字符串转换为字节数组的操作。在这个场景中，它的作用是将字符串 "123456" 转换为字节数组，以便后续进行密码加密操作。
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        //设置当前记录的创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //设置当前记录创建人id和修改id
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        //调用持久层
+        employeeMapper.insert(employee);
+
     }
 
 }

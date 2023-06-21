7db5006d0107845f760d9e62c218b4630e726ae8
wangsong <384097785@qq.com>
2023-06-21 23:11:04 +0800

    实现编辑员工信息业务

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -35,8 +35,15 @@ public interface EmployeeMapper {
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     /**
-     * 启用禁用员工
+     * 启用禁用员工,更新员工信息
      * @param employee
      */
     void update(Employee employee);
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
